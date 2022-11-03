72281d88946e76fa3ddd1382b6983b326bffee24
Anas Qazza <anasqazza@gmail.com>
2022-11-03 13:23:27 -0700

    Added code to javascript such as eventlistener and local storage time blocks

---
+++ javascript.js
@@ -0,0 +1,29 @@
+var todayDate = moment().format("YYYY-MM-DD HH:mm:ss");
+$("#currentDay").html(todayDate);
+
+$(function () {
+  // saveButton click listener - this is the same as "eventListener"
+  $(".saveButton").on("click", function () {
+    // Get nearby values of the description in JQuery
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    // Save text in local storage
+    localStorage.setItem(time, text);
+  });
+
+  // Get item from local storage, display to time block
+
+  $("#hour9 .description").val(localStorage.getItem("hour9"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+  $("#hour18 .description").val(localStorage.getItem("hour18"));
+
+  timeTracker();
+});
